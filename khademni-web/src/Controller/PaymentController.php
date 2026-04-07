335b846ad3722bd25d4d0dbe177b1f5324442c04
ayoub <ayoub.dakhli@esprit.tn>
2026-04-07 20:26:20 +0100

    crud zid checki y raslen kbal mtmergi

---
+++ khademni-web/src/Controller/PaymentController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PaymentRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/payment')]
+class PaymentController extends AbstractController
+{
+    #[Route('/', name: 'app_payment_index', methods: ['GET'])]
+    public function index(PaymentRepository $paymentRepository): Response
+    {
+        return $this->render('payment/index.html.twig', [
+            'payments' => $paymentRepository->findAll(),
+        ]);
+    }
+}
