02c9fd72807de51cbe9b4174dcec20a1dc32282e
rg-esprit <guembrk.raslen@esprit.tn>
2026-05-06 16:59:38 +0100

    yassine fixes

--- khademni-web/src/Service/NotificationService.php
+++ khademni-web/src/Service/NotificationService.php
@@ -5,11 +5,10 @@ namespace App\Service;
 use App\Entity\Notification;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
-use Psr\Log\LoggerInterface;
 
 class NotificationService
 {
-    public function __construct(private EntityManagerInterface $em, private LoggerInterface $logger) {}
+    public function __construct(private EntityManagerInterface $em) {}
 
     public function send(User $recipient, string $message, string $type = 'info', ?string $link = null): void
     {
@@ -19,15 +18,7 @@ class NotificationService
         $notification->setType($type);
         $notification->setLink($link);
 
-        try {
-            $this->em->persist($notification);
-            $this->em->flush();
-        } catch (\Throwable $exception) {
-            $this->logger->error('Unable to save notification', [
-                'recipient' => $recipient->getId(),
-                'message' => $message,
-                'error' => $exception->getMessage(),
-            ]);
-        }
+        $this->em->persist($notification);
+        $this->em->flush();
     }
 }
